f6e2277f5169e2da09121a47c605cad40f79232a
JoshMyman1 <97846842+JoshMyman1@users.noreply.github.com>
2022-05-31 19:51:11 -0400

    Add files via upload
    Encryption using ECDSA

---
+++ sign.py
@@ -0,0 +1,31 @@
+from fastecdsa.curve import secp256k1
+from fastecdsa.keys import export_key, gen_keypair
+
+from fastecdsa import curve, ecdsa, keys, point
+from hashlib import sha256
+
+def sign(m):
+	#generate public key
+	#Your code here
+
+	private_key, public_key = keys.gen_keypair(curve.secp256k1)
+	r, s = ecdsa.sign(m, private_key)
+
+	
+	encrypted_message = []
+	encrypted_message.append(r)
+	encrypted_message.append(s)
+
+
+	assert isinstance( public_key, point.Point )
+	assert isinstance( r, int )
+	assert isinstance( s, int )
+	return( public_key, [r,s] )
+
+
+def main():
+	sign("m")
+
+
+if __name__ == '__main__':
+    main()
