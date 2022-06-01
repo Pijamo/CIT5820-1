0496a645c1f3aeab4b25c7db0f3e4b8143a0391e
JoshMyman1 <97846842+JoshMyman1@users.noreply.github.com>
2022-05-31 20:08:07 -0400

    Encryption using ECDSA

--- sign.py
+++ sign.py
@@ -11,12 +11,6 @@ def sign(m):
 	private_key, public_key = keys.gen_keypair(curve.secp256k1)
 	r, s = ecdsa.sign(m, private_key, curve=curve.secp256k1)
 
-
-	encrypted_message = []
-	encrypted_message.append(r)
-	encrypted_message.append(s)
-
-
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
 	assert isinstance( s, int )
