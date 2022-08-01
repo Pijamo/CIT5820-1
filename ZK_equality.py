d35f08f056d1aa54b76782bc6e45df3793cdd53e
JoshMyman1 <97846842+JoshMyman1@users.noreply.github.com>
2022-08-01 16:34:13 -0400

    Add files via upload

--- ZK_equality.py
+++ ZK_equality.py
@@ -5,20 +5,20 @@ from zksk import utils
 def ZK_equality(G, H):
     # Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
     # G, H = utils.make_generators(num=2, seed=42)
-    r1 = Secret(utils.get_random_num(bits=128))
-    r2 = Secret(utils.get_random_num(bits=128))
+    r_1 = Secret(utils.get_random_num(bits=128))
+    r_2 = Secret(utils.get_random_num(bits=128))
     m = Secret(utils.get_random_num(bits=128))
 
-    C1 = r1.value * G
-    C2 = r1.value * H + m.value * G
-    D1 = r2.value * G
-    D2 = r2.value * H + m.value * G
+    C1 = r_1.value * G
+    C2 = r_1.value * H + m.value * G
+    D1 = r_2.value * G
+    D2 = r_2.value * H + m.value * G
 
-    stmt = DLRep(C1, r1 * G) & DLRep(C2, r1 * H + m * G) & DLRep(D1, r2 * G) & DLRep(D2, r2 * H + m * G)
-    zk_proof = stmt.prove()
+    st = DLRep(C1, r_1 * G) & DLRep(C2, r_1 * H + m * G) & DLRep(D1, r_2 * G) & DLRep(D2, r_2 * H + m * G)
+    zkp = st.prove()
 
     # Generate a NIZK proving equality of the plaintexts
-    stmt.verify(zk_proof)
+    st.verify(zkp)
 
     # Return two ciphertexts and the proof
-    return (C1, C2), (D1, D2), zk_proof
+    return (C1, C2), (D1, D2), zkp
